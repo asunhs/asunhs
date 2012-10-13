691d1a0f7d4af7882939c5c6003b79b9f70a33be
asunhs <asunhs@gmail.com>
2012-10-13 17:06:20 +0900

    change

--- Laboratory/src/org/asunhs/laboratory/MyClass.java
+++ Laboratory/src/org/asunhs/laboratory/MyClass.java
@@ -2,6 +2,23 @@ package org.asunhs.laboratory;
 
 public class MyClass {
 	public static void main(String[] args) {
+		MyClass my = new MyClass();
 		System.out.println("Hello GIT!");
+		
+		String s = "Hello";
+		String b = my.pass2(s);
+		
+		int i = 3;
+		int j = my.pass2(i);
+		
+		
+	}
+	
+	public <T> T pass(T t) {
+		return t;
+	}
+	
+	public <T> T pass2(Object obj) {
+		return (T)obj;
 	}
 }
