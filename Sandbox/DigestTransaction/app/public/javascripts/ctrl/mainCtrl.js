ac83b14bf185856bc7d8a5c73f86151308bcbfe1
Hyunsang Sun <asunhs@gmail.com>
2014-08-14 19:31:43 +0900

    ui prototype

--- Sandbox/DigestTransaction/app/public/javascripts/ctrl/mainCtrl.js
+++ Sandbox/DigestTransaction/app/public/javascripts/ctrl/mainCtrl.js
@@ -4,10 +4,36 @@
     angular.module('DutchPayApp')
     .controller('MainCtrl', function ($scope, ReceiptsSvc, Receipts) {
         
+        var isWriting = $scope.isWriting = function (receipt) {
+            return receipt.status === 'writing';
+        }
+        
         ReceiptsSvc.pullReceipts();
         
         $scope.welcome = "Welcome!";
         
         $scope.receipts = Receipts;
+        
+        $scope.addTransaction = function (receipt) {
+            
+            if (!isWriting(receipt)) {
+                return;
+            }
+            
+            if (!receipt.transactions) {
+                receipt.transactions = [];
+            }
+            
+            receipt.transactions.push({});
+        };
+        
+        $scope.addReceipt = function (receipts) {
+            
+            if (!receipts) {
+                return;
+            }
+            
+            receipts.push(ReceiptsSvc.newReceipt());
+        }
     });
 }());
--- Sandbox/DigestTransaction/app/public/javascripts/svc/receiptsSvc.js
+++ Sandbox/DigestTransaction/app/public/javascripts/svc/receiptsSvc.js
@@ -4,17 +4,42 @@
     angular.module('DutchPayApp')
     .service('ReceiptsSvc', function ($http, Receipts) {
         
-        var svc = this;
+        var svc = this,
+            uuid = function() {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                    return v.toString(16);
+                });
+            };
         
-        this.getReceipt = function (receiptId) {
-            return _.find(Receipts, function (receipt) {
-                return receipt.receiptId == receiptId;
-            });
+        this.newReceipt = function (receipt) {
+            return _.extend({
+                status : 'writing',
+                temporary : uuid()
+            }, receipt);
+            
+        };
+        
+        this.getReceipt = function (condition) {
+            
+            if (condition.temporary) {
+                return _.find(Receipts, function (receipt) {
+                    return receipt.temporary == condition.temporary;
+                });
+            }
+            
+            if (condition.receiptId) {
+                return _.find(Receipts, function (receipt) {
+                    return receipt.receiptId === condition.receiptId;
+                });
+            }
+            
+            return {};
         };
         
         this.addReceipt = function (newReceipt) {
             
-            var existed = svc.getReceipt(newReceipt.receiptId);
+            var existed = svc.getReceipt(newReceipt);
             
             if (!!existed) {
                 Receipts[_.indexOf(Receipts, existed)] = newReceipt;
@@ -22,48 +47,52 @@
                 Receipts.push(newReceipt);
             }
             
-        }
+        };
         
         this.addReceipts = function (newReceipts) {
             _.each(newReceipts, svc.addReceipt);
-        }
+        };
+        
+        this.pushReceipts = function (receipts) {
+            
+        };
         
         this.pullReceipts = function () {
             
             svc.addReceipts([
                 {
-                    receiptId : 1,
+                    to : 'A',
+                    receiptId : 2,
                     title : 'Korean Food',
                     transactions : [
                         {
                             from : 'B',
-                            to : 'A',
                             amount : 4000
                         },
                         {
                             from : 'C',
-                            to : 'A',
                             amount : 3000
                         }
                     ],
-                    createTimestamp : 1407493380394
+                    createdTimestamp : 1407493380394,
+                    status : 'writing'
                 },
                 {
-                    receiptId : 0,
+                    to : 'B',
+                    receiptId : 1,
                     title : 'Italian Food',
                     transactions : [
                         {
                             from : 'A',
-                            to : 'B',
                             amount : 2000
                         },
                         {
                             from : 'C',
-                            to : 'B',
                             amount : 3000
                         }
                     ],
-                    createTimestamp : 1407493423802
+                    createdTimestamp : 1407493423802,
+                    status : 'yet'
                 }
             ]);
         };
